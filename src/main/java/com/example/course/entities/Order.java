4067f9f025fee8e36c2c0e4f1f22eadcf4197d7a
RichardFelipe-collab <richardfelipe.123@hotmail.com>
2021-01-06 12:52:55 -0300

    Order status enum

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.example.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 
@@ -28,6 +29,8 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderstatus;
+	
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private User client;
@@ -36,10 +39,11 @@ public class Order implements Serializable{
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderstatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderstatus);
 		this.client = client;
 	}
 
@@ -57,6 +61,16 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderstatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderstatus) {
+		if (orderstatus != null) {
+		this.orderstatus = orderstatus.getCode(); 
+		}
 	}
 
 	public User getClient() {
