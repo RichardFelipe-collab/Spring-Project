d179c358e4341ad38871cb5770eb147d63df146c
RichardFelipe-collab <richardfelipe.123@hotmail.com>
2021-02-20 18:23:58 -0300

    Parte 2

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.course.entities.Category;
 import com.example.course.entities.Order;
+import com.example.course.entities.OrderItem;
 import com.example.course.entities.Product;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
 import com.example.course.repositories.CategoryRepository;
+import com.example.course.repositories.OrderItemRepository;
 import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.ProductRepository;
 import com.example.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -45,18 +50,19 @@ public class TestConfig implements CommandLineRunner{
 		Product p2 = new Product(null, "Cem anos de Solidão", "Amet ",100.5, "");
 		Product p3 = new Product(null, "PC Gamer", "Lorem Ipsum",2000.5, "");
 		Product p4 = new Product(null, "Mac Book Pro", "Lorem Ipsum",3000.5, "");
+		Product p5 = new Product(null, "Lenovo", "Lorem Ipsum",5000.5, "");
 		
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
-		productRepository.saveAll(Arrays.asList(p1,p2));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
-		p1.getCategories().add(cat3);
+		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
-		p3.getCategories().add(cat2);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat2);
+		p5.getCategories().add(cat2);
 		
-		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4));
-		
-		
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -64,11 +70,17 @@ public class TestConfig implements CommandLineRunner{
 		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.SHIPPED, u1);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT , u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
+		OrderItem oi1 = new OrderItem(o1,p1,2,p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1,p3,1,p4.getPrice());
+		OrderItem oi3 = new OrderItem(o2,p3,2,p1.getPrice());
+		OrderItem oi4 = new OrderItem(o3,p5,2,p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 	}
 }
--- src/main/java/com/example/course/entities/OrderItem.java
+++ src/main/java/com/example/course/entities/OrderItem.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.example.course.entities.pk.OrderItemPk;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order_item")
@@ -15,7 +16,8 @@ public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private OrderItemPk id;
+	private OrderItemPk id = new OrderItemPk();
+	 
 	private Integer quantity;
 	private Double price;
 	
@@ -39,6 +41,7 @@ public class OrderItem implements Serializable {
 		id.setProduct(product);
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
